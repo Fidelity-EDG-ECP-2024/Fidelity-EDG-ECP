e546d7accd380bfbd42d1c2b224079c0abcac166
Goulda <36556466+Goulda@users.noreply.github.com>
2024-09-21 14:03:49 -0400

    Initial frontend prototype grid
    Implemented initial frontend prototype

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+
+export const routes: Routes = [{
+  path: 'get_data', component: AppComponent,
+
+  title: 'Student List',
+}];
